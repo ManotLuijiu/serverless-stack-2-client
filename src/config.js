62fabc48f316cdad701bd2a180be30e008ecb650
ManotLuijiu <manot_luijiu@hotmail.com>
2018-08-31 16:22:29 +0700

    Adding Stripe keys to config

--- src/config.js
+++ src/config.js
@@ -1,41 +1,41 @@
 const dev = {
+  STRIPE_KEY: 'pk_test_zhfW2t1gkoFmcNmGkQF1QoAd',
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-2-api-dev-attachmentsbucket-us6dffs3seie"
+    REGION: 'us-east-1',
+    BUCKET: 'notes-app-2-api-dev-attachmentsbucket-us6dffs3seie'
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://61l3miapli.execute-api.us-east-1.amazonaws.com/dev"
+    REGION: 'us-east-1',
+    URL: 'https://61l3miapli.execute-api.us-east-1.amazonaws.com/dev'
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_P2JUaERrX",
-    APP_CLIENT_ID: "2frstskdhccbtjt31t634b13a6",
-    IDENTITY_POOL_ID: "us-east-1:acd46d04-5bf9-422a-a6b0-ef9b383391e5"
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_P2JUaERrX',
+    APP_CLIENT_ID: '2frstskdhccbtjt31t634b13a6',
+    IDENTITY_POOL_ID: 'us-east-1:acd46d04-5bf9-422a-a6b0-ef9b383391e5'
   }
 };
 
 const prod = {
+  STRIPE_KEY: 'pk_test_zhfW2t1gkoFmcNmGkQF1QoAd',
   s3: {
-    REGION: "us-east-1",
-    BUCKET: "notes-app-2-api-prod-attachmentsbucket-1ffikgldsmnjd"
+    REGION: 'us-east-1',
+    BUCKET: 'notes-app-2-api-prod-attachmentsbucket-1ffikgldsmnjd'
   },
   apiGateway: {
-    REGION: "us-east-1",
-    URL: "https://sg9f7jhdhb.execute-api.us-east-1.amazonaws.com/prod"
+    REGION: 'us-east-1',
+    URL: 'https://sg9f7jhdhb.execute-api.us-east-1.amazonaws.com/prod'
   },
   cognito: {
-    REGION: "us-east-1",
-    USER_POOL_ID: "us-east-1_5A1E73tt1",
-    APP_CLIENT_ID: "son7j3jvtafm7hdk8jqp2p5k8",
-    IDENTITY_POOL_ID: "us-east-1:be8384b4-2231-4950-aeea-7070abf84649"
+    REGION: 'us-east-1',
+    USER_POOL_ID: 'us-east-1_5A1E73tt1',
+    APP_CLIENT_ID: 'son7j3jvtafm7hdk8jqp2p5k8',
+    IDENTITY_POOL_ID: 'us-east-1:be8384b4-2231-4950-aeea-7070abf84649'
   }
 };
 
 // Default to dev if not set
-const config = process.env.REACT_APP_STAGE === 'prod'
-  ? prod
-  : dev;
+const config = process.env.REACT_APP_STAGE === 'prod' ? prod : dev;
 
 export default {
   // Add common config values here
