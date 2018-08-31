1582f46a84ac2e2db85b840340dd86340ebd497e
ManotLuijiu <manot_luijiu@hotmail.com>
2018-08-31 18:19:10 +0700

    Committing a typo

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -1,8 +1,8 @@
-import React, { Component } from "react";
-import { API } from "aws-amplify";
-import { Link } from "react-router-dom";
-import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
-import "./Home.css";
+import React, { Component } from 'react';
+import { API } from 'aws-amplify';
+import { Link } from 'react-router-dom';
+import { PageHeader, ListGroup, ListGroupItem } from 'react-bootstrap';
+import './Home.css';
 
 export default class Home extends Component {
   constructor(props) {
@@ -30,35 +30,37 @@ export default class Home extends Component {
   }
 
   notes() {
-    return API.get("notes", "/notes");
+    return API.get('notes', '/notes');
   }
 
   handleNoteClick = event => {
     event.preventDefault();
-    this.props.history.push(event.currentTarget.getAttribute("href"));
-  }
+    this.props.history.push(event.currentTarget.getAttribute('href'));
+  };
 
   renderNotesList(notes) {
     return [{}].concat(notes).map(
       (note, i) =>
-        i !== 0
-          ? <ListGroupItem
-              key={note.noteId}
-              href={`/notes/${note.noteId}`}
-              onClick={this.handleNoteClick}
-              header={note.content.trim().split("\n")[0]}
-            >
-              {"Created: " + new Date(note.createdAt).toLocaleString()}
-            </ListGroupItem>
-          : <ListGroupItem
-              key="new"
-              href="/notes/new"
-              onClick={this.handleNoteClick}
-            >
-              <h4>
-                <b>{"\uFF0B"}</b> Create a new note
-              </h4>
-            </ListGroupItem>
+        i !== 0 ? (
+          <ListGroupItem
+            key={note.noteId}
+            href={`/notes/${note.noteId}`}
+            onClick={this.handleNoteClick}
+            header={note.content.trim().split('\n')[0]}
+          >
+            {'Created: ' + new Date(note.createdAt).toLocaleString()}
+          </ListGroupItem>
+        ) : (
+          <ListGroupItem
+            key="new"
+            href="/notes/new"
+            onClick={this.handleNoteClick}
+          >
+            <h4>
+              <b>{'\uFF0B'}</b> Create a new note
+            </h4>
+          </ListGroupItem>
+        )
     );
   }
 
@@ -66,7 +68,7 @@ export default class Home extends Component {
     return (
       <div className="lander">
         <h1>Scratch</h1>
-        <p>A simple note taking app</p>
+        <p>A very expensive note taking app</p>
         <div>
           <Link to="/login" className="btn btn-info btn-lg">
             Login
